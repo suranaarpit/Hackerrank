b00eb017a0c2b710e2f09a149c471dd2abe1f346
Arpit Surana <suranaarpit34@gmail.com>
2022-07-14 15:17:05 +0530

    hackerrank sol.

---
+++ Picking Numbers/sol.js
@@ -0,0 +1,55 @@
+Question: "https://www.hackerrank.com/challenges/picking-numbers/problem?h_r=profile"
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+    let MAX_COUNT = 0;
+    [...new Set(a)].forEach(x => {
+        MAX_COUNT = Math.max(MAX_COUNT,
+            a.reduce((c, v) => c += (v === x || v === x + 1), 0));
+    })
+    return MAX_COUNT;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
