29602b63db5fe70b6615eb5f912f47b289c56f13
Arpit Surana <suranaarpit34@gmail.com>
2022-07-15 20:48:25 +0530

    hackerrank sol.

---
+++ Breaking the Records/Sol.js
@@ -0,0 +1,57 @@
+Q: "https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem"
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores) {
+    // Write your code here
+ var h = [scores[0]]; 
+    var l = [scores[0]];
+    scores.forEach( e=> {
+        if (e>h[h.length-1]) h.push(e);
+        if (e<l[l.length-1]) l.push(e);
+    });
+    return [h.length-1,l.length-1];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
